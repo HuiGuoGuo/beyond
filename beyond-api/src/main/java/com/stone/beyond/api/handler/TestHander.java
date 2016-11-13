200769d0675671f9e9897d0781d3d369a40808f5
wanghui <wanghui@simplecredit.com>
2016-11-13 16:02:48 +0800

    配置Logback

---
+++ beyond-api/src/main/java/com/stone/beyond/api/handler/TestHander.java
@@ -0,0 +1,19 @@
+package com.stone.beyond.api.handler;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+/**
+ * Created by stone on 2016/11/13.
+ */
+@Controller
+public class TestHander {
+    Logger Log = LoggerFactory.getLogger(TestHander.class);
+
+    @RequestMapping("/test")
+    public void test(){
+        Log.info("Logback Helloworld!");
+    }
+}
