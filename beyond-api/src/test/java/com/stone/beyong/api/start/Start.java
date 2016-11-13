0410558b2980f5c1b0433bb6acf1a9147854d4d7
wanghui <wanghui@simplecredit.com>
2016-11-13 15:50:29 +0800

    配置嵌入式Tomcat

--- beyond-api/src/test/java/com/stone/beyong/api/start/Start.java
+++ beyond-api/src/test/java/com/stone/beyong/api/start/Start.java
@@ -2,7 +2,7 @@ package com.stone.beyong.api.start;
 public class Start {
 
 	public static void main( String[] args ) throws Exception {
-		TomcatBootstrap.start( 8081, Profiles.TEST);
+		TomcatBootstrap.start( 8081, Profiles.LOCAL);
 	}
 
 }	
