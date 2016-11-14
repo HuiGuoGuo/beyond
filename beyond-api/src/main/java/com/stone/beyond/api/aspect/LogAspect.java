295dad1ddde85ec6b00a0c1df96324a8d9f39d83
wanghui <wanghui@simplecredit.com>
2016-11-14 13:36:00 +0800

    配置Logback

---
+++ beyond-api/src/main/java/com/stone/beyond/api/aspect/LogAspect.java
@@ -0,0 +1,43 @@
+package com.stone.beyond.api.aspect;
+
+import com.alibaba.fastjson.JSON;
+import org.aspectj.lang.ProceedingJoinPoint;
+import org.aspectj.lang.annotation.Around;
+import org.aspectj.lang.annotation.Aspect;
+import org.aspectj.lang.annotation.Pointcut;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.stereotype.Component;
+
+/**
+ * Created by stone on 2016/11/14.
+ */
+
+@Aspect
+@Component
+public class LogAspect {
+
+   private Logger Log = LoggerFactory.getLogger(LogAspect.class);
+
+    /**
+     * 切入点
+     */
+    @Pointcut(value = "execution (* com.stone.beyond.api.handler..*.*(..))")
+    public void pointcut() {
+    }
+
+
+    @Around(value = "com.stone.beyond.api.aspect.LogAspect.pointcut()")
+    public Object process(ProceedingJoinPoint jp) throws java.lang.Throwable {
+        Object rvt;
+        long startTime = System.currentTimeMillis();
+        String methodInfo = jp.getTarget().getClass().getSimpleName() + "." + jp.getSignature().getName();
+        Log.info("调用方法:" + methodInfo);
+        rvt = jp.proceed();
+        long endTime = System.currentTimeMillis();
+        String returnInfo = JSON.toJSONString(rvt);
+        long time = endTime - startTime;
+        Log.info("{调用方法:" + methodInfo + ",  返回值:" + returnInfo + ",  耗时:" + time + "毫秒}");
+        return rvt;
+    }
+}
